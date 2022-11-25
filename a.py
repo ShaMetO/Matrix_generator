8636dc3b224ee4026d2048f1a68666b9e1fc5baf
Rachit Jasoria <64753500+Rachit333@users.noreply.github.com>
2022-11-25 13:11:00 +0530

    Update a.py
    Finall changes

--- a.py
+++ a.py
@@ -1,35 +1,60 @@
-import numpy as np
-
-n=int(input("Enter Order of Matrix: "))
-f=1
-while f==1:
-    a = np.random.randint(10, size=(n,n))
-
-    sum1=0
-    sum2=0
+def forEvenNumber(n):
+    arr = [[(n * y) + x + 1 for x in range(n)] for y in range(n)]
+    for i in range(0, n // 4):
+        for j in range(0, n // 4):
+            arr[i][j] = (n * n + 1) - arr[i][j];
+    for i in range(0, n // 4):
+        for j in range(3 * (n // 4), n):
+            arr[i][j] = (n * n + 1) - arr[i][j];
+    for i in range(3 * (n // 4), n):
+        for j in range(0, n // 4):
+            arr[i][j] = (n * n + 1) - arr[i][j];
+    for i in range(3 * (n // 4), n):
+        for j in range(3 * (n // 4), n):
+            arr[i][j] = (n * n + 1) - arr[i][j];
+    for i in range(n // 4, 3 * (n // 4)):
+        for j in range(n // 4, 3 * (n // 4)):
+            arr[i][j] = (n * n + 1) - arr[i][j];
+    print("\nSum of all row, column and diagonals = ",
+          n * (n * n + 1) // 2, "\n")
     for i in range(n):
         for j in range(n):
-            if i==j:
-                sum1+=a[i][j]
-            if i+j==n-1:
-                sum2+=a[i][j]
-    if sum1!=sum2:
-        f=1
-    else:
-        for i in range(n):
-            sumr=0
-            sumc=0
-            for j in range(n):
-                sumr+=a[i][j]
-                sumc+=a[i][j]
-            print(sumr)
-            print(sumc)
-            if sumr!=sum1 and sumc!=sum1:
-                f=1
-            else:
-                f=0
-    if f==0:
-        print("Magic")
-        print(a)
-    else:
-        print("N")   
+            print('%2d ' % (arr[i][j]), end=" ")
+        print()
+def forOddNumber(n):
+    mgsqr = [[0 for x in range(n)]
+             for y in range(n)]
+    r = n // 2
+    c = n - 1
+    num = 1
+    while num <= (n * n):
+        if r == -1 and c == n:
+            c = n - 2
+            r = 0
+        else:
+            if c == n:
+                c = 0
+            if r < 0:
+                r = n - 1
+        if mgsqr[int(r)][int(c)]:
+            c = c - 2
+            r = r + 1
+            continue
+        else:
+            mgsqr[int(r)][int(c)] = num
+            num = num + 1
+        c = c + 1
+        r = r - 1
+    print("\nSum of all row, column and diagonals = ",
+          n * (n * n + 1) // 2, "\n")
+    for i in range(0, n):
+        for j in range(0, n):
+            print('%2d ' % (mgsqr[i][j]), end='')
+        print()
+print("\nWELCOME:)\n")
+n = int(input("Please Enter Number of Rows and Column (n*n): "))
+if n%2==0:
+    forEvenNumber(n)
+else:
+    forOddNumber(n)
+print("\nThank You :)")
